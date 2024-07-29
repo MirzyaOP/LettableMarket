14f5452ae4815c6cf23d2e45dc6188288f1e9f92
MirzyaOP <abdullahcool0000@gmail.com>
2024-07-29 15:54:39 +0700

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ from os import getenv
 
 API_ID = int(getenv("API_ID", 29400566))
 API_HASH = str(getenv("API_HASH", "8fd30dc496aea7c14cf675f59b74ec6f"))
-BOT_TOKEN = str(getenv("BOT_TOKEN", "7413064695:AAEfV9Lg1nfM9_Snb1wF-iZwPiAgKiCDK6E"))
+BOT_TOKEN = str(getenv("BOT_TOKEN", "7432479667:AAEBiHi6iesvB3Gi9mL1cWqtd8i4102T2SA"))
